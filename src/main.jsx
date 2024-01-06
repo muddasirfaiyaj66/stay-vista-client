731eba79b99960a52c482f9d6c2e1bcf8c10db6b
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2024-01-06 23:45:27 +0600

    setup auth provider

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,13 @@ import {
 } from "react-router-dom";
 import './index.css'
 import { router } from './routes/Routes';
+import AuthProvider from './providers/AuthProviders';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <RouterProvider router={router}/>
+    <AuthProvider>
+    <RouterProvider router={router}/>
+    </AuthProvider>
+
   </React.StrictMode>,
 )
