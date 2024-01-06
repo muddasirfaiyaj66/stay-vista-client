21e6e8e77288ebbccfd1ce8bc4d5e8e0992b5557
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2024-01-06 23:55:00 +0600

    install react hot toaster, a fake rooms json file , and add favicon image in public folder

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,12 @@ import {
 import './index.css'
 import { router } from './routes/Routes';
 import AuthProvider from './providers/AuthProviders';
+import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
+      <Toaster/>
     <RouterProvider router={router}/>
     </AuthProvider>
 
