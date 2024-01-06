fef5849bf04ea910ac8a9f006516a66123ed5c65
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2024-01-07 00:06:03 +0600

    add footer on Main Layout

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Shared/Navbar/Navbar";
+import Footer from "../Shared/Footer/Footer";
 
 
 const Main = () => {
@@ -9,6 +10,7 @@ const Main = () => {
        <div className='pt-24 min-h-[calc(100vh-68px)]'>
         <Outlet />
       </div>
+      <Footer></Footer>
         </div>
     );
 };
