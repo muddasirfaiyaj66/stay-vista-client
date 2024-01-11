9e379b765df80289f495e0acad93a3dcd3a1b79d
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2024-01-11 20:46:46 +0600

    install  react-date-range

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,11 @@
 import { Link } from 'react-router-dom'
 import { FcGoogle } from 'react-icons/fc'
+import useAuth from '../../hooks/useAuth'
 
 const Login = () => {
+  const {signIn}= useAuth()
+
+  
   return (
     <div className='flex justify-center items-center min-h-screen'>
       <div className='flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-100 text-gray-900'>
--- src/Shared/Navbar/MenuDropdown.jsx
+++ src/Shared/Navbar/MenuDropdown.jsx
@@ -1,8 +1,8 @@
 import { AiOutlineMenu } from 'react-icons/ai'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import useAuth from '../../../hooks/useAuth'
-import avatarImg from '../../../assets/images/placeholder.jpg'
+import avatarImg from '../../assets/images/placeholder.jpg'
+import useAuth from '../../hooks/useAuth'
 
 const MenuDropdown = () => {
   const [isOpen, setIsOpen] = useState(false)
