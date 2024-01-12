91bcda25524e4d44fda2a992b6885fbfbe5d59bc
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2024-01-13 01:03:00 +0600

    if no rooms are available a message will show .THis part complete

--- src/components/Rooms/Rooms.jsx
+++ src/components/Rooms/Rooms.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Card from "./Card";
 import Container from "../Container";
 import { useSearchParams } from "react-router-dom";
+import Heading from "../../Shared/Heading/Heading";
 
 
 
@@ -25,9 +26,13 @@ const Rooms = () => {
     },[category])
     return (
         <Container>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          {rooms && rooms.length > 0 ?    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
            {rooms.map(room=> <Card key={room._id} room={room}></Card>)}
-        </div>
+        </div> : 
+        <Heading center={true} title='No Rooms Available In This Category!'
+        subtitle='Please Select Other Categories.'
+        />
+        }
         </Container>
     );
 };
