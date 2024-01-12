70d230721f9b1f16676ad07c8d22822d041fe7cc
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2024-01-13 00:40:53 +0600

    dynamically selected item navigate system complete

--- src/components/Rooms/Categories/CategoryBox.jsx
+++ src/components/Rooms/Categories/CategoryBox.jsx
@@ -2,7 +2,7 @@ import qs from "query-string";
 import { useNavigate, useSearchParams } from "react-router-dom";
 
 
-const CategoryBox = ({label, icon:Icon}) => {
+const CategoryBox = ({label, icon:Icon, selected}) => {
     const [params, setParams ]= useSearchParams()
     const navigate = useNavigate()
     const handleClick =()=>{
@@ -21,7 +21,7 @@ const CategoryBox = ({label, icon:Icon}) => {
         }
     }
     return (
-        <div onClick={handleClick} className="flex flex-col justify-center items-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer ">
+    <div onClick={handleClick} className={`flex flex-col justify-center items-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer ${selected ? 'border-b-neutral-800 text-neutral-800' : ''}`}>
             <Icon size={26}></Icon>
            <div className="text-sm font-medium">
             {label}
