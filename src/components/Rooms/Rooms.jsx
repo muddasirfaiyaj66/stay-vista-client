8c66650c431f6a5b5771a104fab0c4a9c722e646
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2024-01-13 01:12:40 +0600

    loader spinner added in rooms component

--- src/components/Rooms/Rooms.jsx
+++ src/components/Rooms/Rooms.jsx
@@ -3,15 +3,18 @@ import Card from "./Card";
 import Container from "../Container";
 import { useSearchParams } from "react-router-dom";
 import Heading from "../../Shared/Heading/Heading";
+import Loader from "../Loader";
 
 
 
 const Rooms = () => {
     const [rooms,setRooms]=useState([]);
     const [params, setParams] = useSearchParams()
+    const [loading, setLoading]=useState(false)
   const category = params.get('category')
  
     useEffect(()=>{
+        setLoading(true)
         fetch('./rooms.json')
         .then(res=> res.json())
         .then(data=>{
@@ -22,8 +25,10 @@ const Rooms = () => {
             else{
                 setRooms(data)
             }
+            setLoading(false)
         })
     },[category])
+    if(loading) return <Loader></Loader>
     return (
         <Container>
           {rooms && rooms.length > 0 ?    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
