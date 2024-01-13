357a93ed75346f3aec8af58f337084349444476e
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2024-01-13 09:38:37 +0600

    RoomDetails data find by id method complete

--- src/Pages/RoomDetails/RoomDetails.jsx
+++ src/Pages/RoomDetails/RoomDetails.jsx
@@ -1,10 +1,31 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Container from "../../components/Container";
+import Loader from "../../components/Loader";
 
 
 const RoomDetails = () => {
+    const {id}=useParams()
+    const [room,setRoom]=useState({})
+    const [loading,setLoading]=useState(true)
+    //fetching room details
+    useEffect(()=>{
+        fetch('/rooms.json')
+        .then(res=> res.json())
+        .then(data=>{
+            const findRoomData = data.find(item=> item._id === id)
+
+            setRoom(findRoomData)
+            setLoading(false)
+        })
+
+    },[id])
+    if(loading) return <Loader></Loader>
     return (
-        <div>
-            
-        </div>
+        <Container>
+
+            {room?.title}
+        </Container>
     );
 };
 
