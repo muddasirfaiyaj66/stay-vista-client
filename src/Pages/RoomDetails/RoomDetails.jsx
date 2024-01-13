c7abb5554cac08adca68e2026e84e8b1cc7be1b1
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2024-01-13 09:18:28 +0600

    room details component create

---
+++ src/Pages/RoomDetails/RoomDetails.jsx
@@ -0,0 +1,11 @@
+
+
+const RoomDetails = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default RoomDetails;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -4,6 +4,7 @@ import Home from '../Pages/Home/Home'
 import ErrorPage from '../Pages/ErrorPage'
 import Login from '../Pages/Login/Login'
 import SignUp from '../Pages/SignUp/SignUp'
+import RoomDetails from '../Pages/RoomDetails/RoomDetails'
 
 
 export const router = createBrowserRouter([
@@ -16,6 +17,10 @@ export const router = createBrowserRouter([
         path: '/',
         element: <Home></Home>,
       },
+      {
+        path:'/room/:id',
+        element:<RoomDetails></RoomDetails>
+      }
     ],
   },
   { path: '/login', element: <Login /> },
